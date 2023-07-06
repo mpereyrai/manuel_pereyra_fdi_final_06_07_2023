3f6a0ee8b32f0e5d11fe43e2e368e08c39cdeb8e
mpereyrai <134970384+mpereyrai@users.noreply.github.com>
2023-07-06 11:37:48 -0300

    Update exchange_rate.py

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,1 +1,10 @@
+import json
+import requests
+
+def get_xr():
+    url = 'https://api.bluelytics.com.ar/v2/latest'
+    r = requests.get(url)
+    data=json.loads(r.text)
+    x_rate = data['oficial']['value_sell']
+    return x_rate
 
