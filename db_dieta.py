ab0fc514c22551a8b6be2882db7cda435411ade7
mpereyrai <134970384+mpereyrai@users.noreply.github.com>
2023-07-06 11:36:52 -0300

    Update db_dieta.py

--- db_dieta.py
+++ db_dieta.py
@@ -1,1 +1,23 @@
+import sqlite3
+DATABASE_NAME = "dieta.db"
 
+
+def get_db():
+    conn = sqlite3.connect(DATABASE_NAME)
+    return conn
+
+
+def create_tables():
+    tables = [
+        """CREATE TABLE IF NOT EXISTS dieta(
+                id INTEGER PRIMARY KEY,
+                restriction TEXT NOT NULL,
+                restriccion INTEGER NOT NULL,
+                USD REAL NOT NULL
+            )
+            """
+    ]
+    db = get_db()
+    cursor = db.cursor()
+    for table in tables:
+        cursor.execute(table)
